5fa6b55c39814c0b959773da7e221793a61caf08
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-15 09:47:45 +0200

    Deleted todo app code since this project is not configured for it.

--- todo-backend/src/main/java/com/booleanuk/TodoApp/repositories/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booleanuk.TodoApp.repositories;
-
-import com.booleanuk.TodoApp.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
-}
