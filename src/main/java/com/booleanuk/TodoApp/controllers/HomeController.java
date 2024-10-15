876350a8edec5fa779477cd545fe7ccea0f20d10
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-10-15 09:09:17 +0200

    Copied todo-app files

---
+++ src/main/java/com/booleanuk/TodoApp/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.booleanuk.TodoApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Please navigate to /todos");
+    }
+}
