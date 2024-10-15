4c38d65aa8d8ab57bb140749d93393456ab8c6a1
Thomas Wiik <thomas.wiik@no.experis.com>
2024-10-15 09:14:28 +0200

    Added todo-backend

---
+++ todo-backend/src/main/java/com/booleanuk/TodoApp/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.booleanuk.TodoApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> getHome() {
+        return ResponseEntity.ok("Please navigate to http://springtodothomasw-env.eba-vf5ymgp2.eu-west-1.elasticbeanstalk.com/todos");
+    }
+}
