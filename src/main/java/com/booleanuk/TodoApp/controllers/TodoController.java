4fc80d4df7f8f064d5721fd4bf94c1359d0342f0
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-10-16 09:04:20 +0200

    Added screenshots to readme

--- src/main/java/com/booleanuk/TodoApp/controllers/TodoController.java
+++ src/main/java/com/booleanuk/TodoApp/controllers/TodoController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 
-@CrossOrigin
+@EnableWebMvc
 @RestController
+@CrossOrigin
 @RequestMapping("todos")
 public class TodoController {
     @Autowired
